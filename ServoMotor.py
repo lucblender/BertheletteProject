2945f661dc527b9685a4b7d85e948ce29a9afb91
lucblender <lucasbonvin@hotmail.com>
2021-03-12 20:21:00 +0100

    threaded setangle

--- ServoMotor.py
+++ ServoMotor.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 from time import sleep
+import threading
 
 class ServoMotor:
     def __init__(self, SERVOPIN):    
@@ -7,7 +8,7 @@ class ServoMotor:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.SERVOPIN, GPIO.OUT)
         self.pwm=GPIO.PWM(self.SERVOPIN, 50)
-        self.pwm.start(0)
+        self.pwm.start(0)        
     def set_angle(self, angle):
         duty = angle / 18 + 2
         GPIO.output(self.SERVOPIN, True)
@@ -22,3 +23,8 @@ class ServoMotor:
     def release(self):        
         GPIO.output(self.SERVOPIN, False)
         self.pwm.ChangeDutyCycle(0)
+    def threaded_set_angle(self,angle):
+        t = threading.Thread(target=self.set_angle, args=(angle,))
+        t.start()
+        return t
+
